3825c4496c5be45cc7e3f281a8cdcc2aa273ba94
shash <shash@LENOVO>
2024-10-01 20:15:11 +0530

    Iplayable main class

---
+++ ConsoleApp1/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Iplayable videoPlayer = new VideoPlayer();
+            Iplayable musicPlayer = new MusicPlayer();
+
+            videoPlayer.Play(); 
+            musicPlayer.Play(); 
+
+            Console.ReadLine(); 
+        }
+    }
+}
