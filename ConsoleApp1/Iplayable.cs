f041f2d549cef90de0ea79fb081f3a25e332a96a
shash <shash@LENOVO>
2024-10-01 20:13:58 +0530

    Iplayable interface added

---
+++ ConsoleApp1/Iplayable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal interface Iplayable
+    {
+        void Play();
+    }
+
+    public class VideoPlayer : Iplayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing Video");
+        }
+    }
+
+    public class MusicPlayer : Iplayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing Music");
+        }
+    }
+
+}
