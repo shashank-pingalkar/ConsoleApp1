62ce92486a1f2423ee848b7b4ac2c153b9461a14
shash <shash@LENOVO>
2024-10-10 20:58:08 +0530

    exception

---
+++ ConsoleApp1/NevException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    internal class NegativeNumber : Exception
+    {
+        public NegativeNumber(string? message) : base(message)
+        {
+        }
+    }
+
+    internal class NevException
+    {
+
+
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter possitive number");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0)
+                {
+                    throw new NegativeNumber("Negative number are not allowed");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            catch (NegativeNumber ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+    }
+}
